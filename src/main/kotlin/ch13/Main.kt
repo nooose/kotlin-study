7d058944c5e08f887422fefca467ed16cb5838cd
nooose <hd15807@gmail.com>
2023-11-26 16:05:12 +0900

    feat: Data, enum, sealed 예제

---
+++ src/main/kotlin/ch13/Main.kt
@@ -0,0 +1,18 @@
+package ch13
+
+fun main() {
+
+}
+
+class House(
+    private val address: String,
+    private val livingRoom: LivingRoom
+) {
+    // 권장되지 않는 클래스 안 클래스
+    inner class LivingRoom( // 굳이 바깥 클래스를 사용한다면 inner 키워드를 사용한다.
+        private val area: Double
+    ) {
+        val address: String
+            get() = this@House.address
+    }
+}
---
+++ src/main/kotlin/ch14/Main.kt
@@ -0,0 +1,13 @@
+package ch14
+
+fun main() {
+    val personDto1 = PersonDto("이름1", 100)
+    val personDto2 = PersonDto("이름1", 100)
+
+    println(personDto1 == personDto2)
+}
+
+data class PersonDto(
+    val name: String,
+    val age: Int
+)
