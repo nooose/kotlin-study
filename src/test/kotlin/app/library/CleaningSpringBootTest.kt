3c7e35df227cf1f49706fed7d3661dbbbee9b120
nooose <hd15807@gmail.com>
2023-12-03 20:52:46 +0900

    feat: 롤백을 위한 테스트 기능 추가

---
+++ src/test/kotlin/app/library/CleaningSpringBootTest.kt
@@ -0,0 +1,20 @@
+package app.library
+
+import org.junit.jupiter.api.AfterEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.jpa.repository.JpaRepository
+
+@SpringBootTest
+class CleaningSpringBootTest {
+
+    @Autowired
+    private lateinit var repositories: List<JpaRepository<*, *>>
+
+    @AfterEach
+    fun clean() {
+        val currentTimeMillis = System.currentTimeMillis()
+        repositories.forEach { it.deleteAll() }
+        println("소요 시간: ${System.currentTimeMillis() - currentTimeMillis}")
+    }
+}
